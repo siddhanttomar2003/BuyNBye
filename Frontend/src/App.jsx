d0d1b77b1377ce9add81a4db7c769fb1a79eeadd
siddhanttomar2003 <siddhanttomar2003@gmail.com>
2024-12-03 01:39:16 +0530

    nothing new

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Card from './components/Card'
+
 function App() {
   return (
     <div>
        <Navbar></Navbar>
-       <Card></Card>
+      <Card></Card>
        <Footer></Footer>
     </div>
   )
